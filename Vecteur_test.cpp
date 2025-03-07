21b28fc4194b3d92051f4270c89bd1e2fe54ae13
Bensidhoum Nicolas <nicolas.bensidhoum@etu.sorbonne-universite.fr>
2025-03-07 16:27:56 +0100

    add Vecteur_test.cpp

---
+++ Vecteur_test.cpp
@@ -0,0 +1,18 @@
+/**
+ * 
+ * FICHIER TEST DES MÉTHODES DE Coordonnee.hpp
+ * 
+ */
+
+#include "Vecteur.hpp"
+using namespace std; 
+
+
+
+int main(int argc, char* argv[]){
+
+
+
+    return 0; 
+
+}
