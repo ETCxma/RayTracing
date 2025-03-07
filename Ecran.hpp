67fbcb1525fe3fa14d6dde982d19b386a0dc6561
Maxime Cortes <maxime.cortes@etu.sorbonne-universite.fr>
2025-03-07 15:32:25 +0100

    remove useless centre_pixels

--- Ecran.hpp
+++ Ecran.hpp
@@ -13,19 +13,19 @@ class Ecran{
         Ecran(Resolution resolution);
 
         Pixel getPixel(int x, int y);
-        Coordonnee getCentrePixel(int x, int y);
+        // Coordonnee getCentrePixel(int x, int y);
         Rayon getRayon(int x, int y);
         Resolution getResolution();
 
         void SetPixel(int x, int y, Pixel &pixel);
-        void SetCentrePixel(int x, int y, Coordonnee &coordonnee);
+        // void SetCentrePixel(int x, int y, Coordonnee &coordonnee);
         void SetRayon(int x, int y, Rayon &rayon);
 
     private:
 
         Pixel *pixels; // good storage method ?
         Rayon *rayons; // good ?
-        Coordonnee *centre_pixels; // good storage method ? Filled how ?
+        // Coordonnee *centre_pixels; // good storage method ? Filled how ?
         Resolution resolution;
 
 };
