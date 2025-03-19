298744ed70cda3544bdd65bf8d70693b14a33246
Maxime Cortes <maxime.cortes@etu.sorbonne-universite.fr>
2025-03-19 16:01:29 +0100

    fix calculRayons

--- Camera_test.cpp
+++ Camera_test.cpp
@@ -5,7 +5,7 @@ int main(int argc, char* argv[]){
 
     std::cout.precision(std::numeric_limits<double>::max_digits10-1);
 
-    Camera c = Camera(Coordonnee(1,1,1), 1, M_PI_2, 0, Resolution(5, 5));
+    Camera c = Camera(Coordonnee(1,1,1), 1, M_PI_2+M_1_PI, M_PI_2+M_1_PI, Resolution(5, 5));
 
     for(int i = 0; i < c.getResolution().getX(); i++){
         for(int j = 0; j < c.getResolution().getY(); j++){
