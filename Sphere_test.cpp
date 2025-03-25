fd30ce7fb740107dcf401c12edf4d393a4a21dcd
Maxime Cortes <maxime.cortes@etu.sorbonne-universite.fr>
2025-03-25 14:03:52 +0100

    implement intersection for sphere

--- Sphere_test.cpp
+++ Sphere_test.cpp
@@ -1,24 +1,15 @@
-/**
- * 
- * FICHIER TEST DES MÉTHODES DE Sphere.hpp
- * 
-*/
 
 #include "Sphere.hpp"
-#include "Coordonnee.hpp"
-using namespace std;
+
 
 
 int main(){
+    
+    Sphere s = Sphere(Coordonnee(10,3,2), 4.0);
+    Rayon r = Rayon(Coordonnee(0,0,0), Coordonnee(1,0,0.2));
 
-    Coordonnee coor1 (1, 1, 1);
-    Sphere Henri (coor1, (double)10);
-    Henri.affichInfos();
-
-    Henri.setRayon(5);
-    Coordonnee coor2 (3, 3, 4);
-    Henri.setCentre(coor2);
-    Henri.affichInfos();
-
+    std::cout << s.intersection(r, Coordonnee(0,0,0)) << std::endl;
+    
     return 0;
+
 }
