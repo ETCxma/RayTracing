197fc06fa937a631b8b00d3416549e14c7c99ba0
Benjamin Lamon <benjamin.lamon@etu.sorbonne-universite.fr>
2025-04-07 14:49:42 +0200

    test coords triangle

--- Triangle_test.cpp
+++ Triangle_test.cpp
@@ -10,6 +10,13 @@ int main(int argc, char* argv[]){
 
 	Triangle triangle = Triangle(points, mat);
 	
+	//should be 0 0 0
+	std::cout << "Coordonnée du point 1 du triangle : " << "X : " << triangle.getPoint(0).getX() << "; Y : " << triangle.getPoint(0).getY() << "; Z : " << triangle.getPoint(0).getZ() << std::endl;
+	//should be 1 0 0
+	std::cout << "Coordonnée du point 2 du triangle : " << "X : " << triangle.getPoint(1).getX() << "; Y : " << triangle.getPoint(1).getY() << "; Z : " << triangle.getPoint(1).getZ() << std::endl;
+	//should be 0 1 0
+	std::cout << "Coordonnée du point 3 du triangle : " << "X : " << triangle.getPoint(2).getX() << "; Y : " << triangle.getPoint(2).getY() << "; Z : " << triangle.getPoint(2).getZ() << std::endl;
+
 	// Should be sqrt(2) 
 	std::cout << "Aire du triangle: " << triangle.Aire() << std::endl;
 	
